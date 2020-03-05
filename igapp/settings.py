bc8efddb79d37cb11489ee37e22d8f8d848da906
Patrick595 <patrickbuong@gmail.com>
2020-03-05 10:56:45 +0300

    defined a function that will get the posts of the day

--- igapp/settings.py
+++ igapp/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'instagram'
-    'bootsrap3'
+    'instagram',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,29 @@
-from django.http  import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to instagram')
+    return render(request, 'welcome.html')
+
+def news_of_day(request):
+    date = dt.date.today()
+    return render(request, 'insta/today-post.html',{'date': date})
+
+
+
+def past_days_news(request,past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(instagram_of_day)
+
+    return render(request, 'insta/past-post.html', {"date":date})
+    
