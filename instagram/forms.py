a6b39c9d533c2b9622c55cd8a6ace4019eb9939f
Patrick595 <patrickbuong@gmail.com>
2020-03-06 12:01:47 +0300

    defined a function that saves the credentials of the user

--- instagram/forms.py
+++ instagram/forms.py
@@ -14,4 +14,14 @@ class NewPostForm(forms.Model):
         exclude = ['upload_by', 'pub_date', 'likes', 'location']
 
 class ProfileForm(forms.Modelform):
- 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['full_name'].widget=forms.TextInput()
+    class Meta
+        model = Profile
+        exclude=['username']
+
+
+class LikeForm(forms.Modelform):
+    class Meta:
+        exclude=['username', 'post']           
--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.db
+
+# Create your models here
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+
+def __str__(self):
+    return self.location_name
+
+def save_location(self):
+    self(save)
