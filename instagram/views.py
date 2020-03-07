b211658a4645d8ccdcf2c707b68deadfcf1af597
Patrick595 <patrickbuong@gmail.com>
2020-03-07 13:08:32 +0300

    created views.py and defined a function that will get the likes,profiles and comments of the users

--- instagram/views.py
+++ instagram/views.py
@@ -3,27 +3,62 @@ from django.http import HttpResponse, Http404
 import datetime as dt
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
+@login_required(login_url='/accounts/login')
+def timeline(rquest):
+    posts= Post.objects.all().order_by("-id")
+    profiles=Profile.objects.all()
+    current_user = request.current_user
 
-def instagram_of_day(request):
-    date = dt.date.today()
-    return render(request, 'insta/today-post.html',{'date': date})
+    comments=Comment.objects.all()
+    likes = Like.objects.all()
 
+    for post in posts:
+        num_likes=0
+        for like in likes:
+            if post.id == like.post.id:
+                num_like +=1
+        posts.likes = num_likes
+        post.save()
 
+    if request.method == 'POST' and 'likez' in request.POST:
+        post_id = request.POST.get("likez")
+        likeform = LikeForm(request.POST)
+        if likeform.is_valid():
+            post_id = int(request.POST.get(likez)) 
+            post = Post.objects.get(id = post_id)
+            like = likeform.save(commit=False)
+            like.username = request.user
+            like.post = post
+            print("like saved")
 
-def past_days_news(request,past_date):
-    
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
+        return redirect("timeline")
+    else:
+        likeform = LikeForm():
 
-    if date == dt.date.today():
-        return redirect(instagram_of_day)
+    if request.method == 'Post' and 'unlikez' in request.POST:
+        post_id = request.POST.get("unlikez")
+        post = Post.objects.get(pk=post_id)
+        control = str(request.user.id)+"-"+str(post.id)
+        like_delete = Like.objects.get(control=control)
+        like_delete.delete()
 
-    return render(request, 'insta/past-post.html', {"date":date})
-    
+        if request.method == 'POST'
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                post_id = int(request.POST.get("idpost"))
+                post = Post.objects.get(id = post_id)
+                comment = form.save(commit=false)
+                comment.username = request.user
+                comment.post = post
+                comment.save()
+            return redirect('timeline')
+
+        else:
+            form = CommentForm()
+
+        posts = Posts.objects.all().order_by("-id")
+        likes = Like.objects.all()
+        Likezz = Like.objects.values_list('control', flat=True)
+        likezz = list(likezz) 
+
+        return render(request,)                          
