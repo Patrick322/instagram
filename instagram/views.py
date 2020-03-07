47700470c844da4da9bbc827e54bb4cb0c786b3d
Patrick595 <patrickbuong@gmail.com>
2020-03-08 00:58:12 +0300

    defined a function that will be responsible for editing the user profile

--- instagram/views.py
+++ instagram/views.py
@@ -4,7 +4,7 @@ import datetime as dt
 
 # Create your views here.
 @login_required(login_url='/accounts/login')
-def timeline(rquest):
+def timeline(request):
     posts= Post.objects.all().order_by("-id")
     profiles=Profile.objects.all()
     current_user = request.current_user
@@ -61,4 +61,103 @@ def timeline(rquest):
         Likezz = Like.objects.values_list('control', flat=True)
         likezz = list(likezz) 
 
-        return render(request,)                          
+        return render(request,)    
+
+
+@login_required(login_url='/account/login')
+def search_results(request)
+if 'search' in request.GET and request.GET["search"]:
+    search_term = request.GET.get("search")
+    searched_users = user.search_user(search_term)
+    messages = f"{search}"
+
+
+    return render(request,)
+
+else:
+    messages="You have not searche for any term."
+    return render(request, )
+
+
+@login_required(login_url='/accounts/login')
+def explore(request):
+    posts = Posts.objects.all()
+    profiles = Profile.objects.all[:5]
+
+
+    return render(request)
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request,id)
+user_object = request.user
+current_user = Profile.objects.get(username__id=request.user.id)
+user = profile.objects.get(username__id=id)
+posts = Post.objects.filter(upload_by = user)
+follows = Folow.objects.all()
+
+if request.method == 'POST' and 'follower' in request.POST:
+    print("follow saved")
+    followed_user_id = request.POST.get("follower")
+    followform = FollowForm(request.POST)
+    if followform.is_valid():
+        followed_user_id = int(request.POST.get("follower"))
+        current_user = Profile.objects.get(username__id=request.user.id)
+        follow.username = request.user
+        followed_user = user.objects.get(pk=followed_user_id)
+        print(followed_user)
+        follow.followed = followed_user
+        follow.follow_id = str(follow.username.id)+"-"+str(follow.followed.id)
+        follow.save()
+        print("follow saved")
+
+        return redirect("profile",username.id)
+    else:
+        followform = FollowForm()    
+
+    if request.method =='POST' and 'unfollower' in request.POST:
+        followed_user_id = request.POST.get("unfollower")
+        followed_user = user.objects.get(pk=followed_user_id)
+        follow_delete = Follow.objects.get(follow_id=follow_id)
+        follow_delete.delete()
+
+
+    follows = Follow.objects.all()
+    followzz = Follow.objects.values_list('follow_id',flat=True)
+    followzz = list(followzz)
+    follower = 0
+    following = 0
+    for follow in followzz:
+        follow = follow.split("-")
+        if follow[0] == str(user.username.id):
+            following+=1
+        if follow[-1] == str(user.username_id)
+            follower+=1
+
+    return render(request, "profile.html")
+
+
+    def following(request):
+        if request.method == 'POST' and 'follower' in request.POST:
+            print("follow saved")
+
+@login_required(login_url='accounts/login')
+def new_post(request):
+    current_user = Profile.objects.get(username__id=request.user.id)
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.upload_by = current_user
+            post.save()
+        return redirect('timeline')
+
+    else:
+        form = NewPostForm()
+        return render(request, 'new_post.html', {"form":"form"})      
+
+@login_required(login_url='accounts/login')
+def edit_profile(request)     
+
+
+
